6f31b25e07fe93f1b469005b497811c485b05aac
Israel Adefidipe <adefidipei@gmail.com>
2025-07-09 13:32:14 +0100

    Remove favicon.ico and update RootLayout to use logo.svg as the new favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </head>
       <body className={`${satoshi.variable} ${clashGrotesk.variable}`}>
         {children}
       </body>
